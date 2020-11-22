86fbb7f2ffe6c0a456a6912cc663c4aadbbde4fd
hillbrianna4 <73502067+hillbrianna4@users.noreply.github.com>
2020-11-21 18:15:56 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = ("express");
 const app = express();
 morgan = require ('morgan');
+bodyParser=require('body-parser'),
+uuid= require('uuid');
 //error handling
 const app = express();
 const bodyParser = require('body-parser'),
@@ -12,43 +14,73 @@ methodOverride = require('method-override');
 let topMovies = [
   {
     title:'Kikis Delivery Service' ,
-    director: '',
+    genre:'anime',
+    data_about_director:{
+    director: 'Hayao Miyazaki',
+    description:,
+  }
   },
+
   { title: 'The Land before Time',
+  genre:'cartoon',
+  data_about_director:{
   director: 'Don Bluth',
-
+}
 },
+
 { title: 'Sixteen Candles',
+genre:'romance',
+data_about_director:{
 director: 'John Hughes',
-
+}
 },
+
 {title:'Heathers',
+genre:'comedy',
+data_about_director:{,
 director: 'Michael Lehmann',
-
-
+}
 },
+
 {title: 'The Breakfast Club',
+genre:'comedy',
+data_about_director:{
 director:'John Hughes',
 }
+},
 
 {title:'The Color Purple',
+genre:'drama',
+data_about_director:{
 director:'Steven Spielberg',
-
+}
 },
+
 {title: 'Weird Science',
-  director:'',
+genre:'comedy','romance',
+data_about_director:{
+director:'John Hughes',
+}
 },
-{title:'Friday the 13th',
-director:'Sean S. Cunningham',
 
+{title:'Friday the 13th',
+genre:'horror',
+data_about_director:{
+director:'Sean S. Cunningham',
+}
 },
 {title: 'Purple Rain',
+genre:'drama',
+data_about_director:{
 director:'Albert Magnoli',
-
+}
 },
 {title: 'Beetlegeuse',
+genre:'comedy',
+data_about_director:{
 director:'Tim Burton',
 
+}
 }
 ];
 
@@ -93,4 +125,54 @@ app.use(methodOverride());
 
 app.use((err, req, res, next) => {
   // logic
+});
+app.use(bodyParser.json());
+
+//Gets the list of data about ALL movies
+app.get('/topMovies'),(req, res) =>{res.json(topMovies);
+});
+
+//gets data about a single movies
+app.get('/topMovies/:title/genre',(req,res) =>{
+  res.json(topMovies.find((topMovies) =>
+{return topMovies.title.genre === req.params.title.genre}));
+});
+
+//gets data about a single director
+app.get('/topMovies/:data_about_director', (req,res)=>{
+res.json(topMovies.find(topMovies)=>
+return topMovies.data_about_director === req.params.data_about_director}));
+)}
+//adds data for a new movie to our list of topMovies
+app.post('/topMovies',(req,res)=>){
+  let newMovie = req.body;
+  if (!newMovie.title){
+    const message = 'Missing title name in request body';
+    res.status(400).send(message);
+  } else{
+    newMovie.title = uuid.v4();
+    topMovies.push(newMovie);
+    res.status(201).send(newMovie);
+  }
+});
+//deletes a movie from our list by ID
+app.delete('/topMovies/:title',(req, res)=>{
+  let topMovies = topMovies.find((topMovies)=>{
+    topMovies.title === req.params.title});
+    if (topMovies){
+      topMovies = topMovies.filter((obj)=>{
+        return obj.title !== req.params.title});
+        res.status(201).send('topMovies '+ req.params.title + ' was deleted.');
+      });
+    }
+});
+// update the user info
+app.put('/topMovies/:topMovies/:title/:genre')(req,res)=>{
+let topMovies = topMovies.find((topMovies)=>{return topMovies.title === req.params.title});
+if (topMovies){
+  topMovies.data_about_director[req.params.data_about_director] = parseInt(req.params.user);
+  res.status(201).send('movie'+ req.params.title+ ' was assigned a grade of '+ req.params.user +' in '+ req.params.data_about_director);
+} else{
+  res.status(404).send('Director with the name '+ req.params.director + ' was not found. ');
+}
 });
